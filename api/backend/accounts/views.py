6b94f413edf26d8fd7756b23b92476fe50c1bad9
Phykin <11149289+phykin@users.noreply.github.com>
2024-11-09 00:06:14 +0100

    redirect user to their respective page

--- api/backend/accounts/views.py
+++ api/backend/accounts/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+
+
+class Login(LoginView):
+    def get_success_url(self):
+        if self.request.user.is_superuser:
+            return reverse_lazy("dm_control_panel")
+        else:
+            return reverse_lazy("player_view")
 
 
 class Registration(TemplateView):

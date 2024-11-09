fdf962c792f1ddf503e65bfbcadb1839796d75a9
Phykin <11149289+phykin@users.noreply.github.com>
2024-11-09 01:00:09 +0100

    remove dnd/ url

--- api/backend/backend/urls.py
+++ api/backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("dnd.urls")),
     path('admin/', admin.site.urls),
     path('auth/', include("accounts.urls")),
 ]

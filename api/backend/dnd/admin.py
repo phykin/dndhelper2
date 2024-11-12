9e07df181927181829f18eb0f2c1282998f2bbad
Phykin <11149289+phykin@users.noreply.github.com>
2024-11-12 20:56:57 +0100

    show artworks and messages in admin view

--- api/backend/dnd/admin.py
+++ api/backend/dnd/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PlayerProfile, Artwork, Message
 
 admin.site.register(PlayerProfile)
+admin.site.register(Artwork)
+admin.site.register(Message)
